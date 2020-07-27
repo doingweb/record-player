67e4605af2fbb126b858ed9b5083cb49b9546362
Chris Antes <chris@chrisdoingweb.com>
2020-07-27 01:26:17 -0700

    Update *.spotify.com cert fingerprint
    Resulted in the following errors in the logs:
    
    ```text
    [18011] Refreshing access token using refresh token: [redacted]
    [HTTP-Client][begin] url: https://accounts.spotify.com/api/token
    [HTTP-Client][begin] host: accounts.spotify.com port: 443 url: /api/token
    [HTTP-Client][sendRequest] type: 'POST' redirCount: 0
    [HTTP-Client] failed connect to accounts.spotify.com:443
    [HTTP-Client][returnError] error(-1): connection refused
    [HTTP-Client][returnError] error(-4): not connected
    [HTTP-Client][end] tcp is closed
    [18026] -1 🌎 POST /api/token
    [18026] ⛔️ connection refused
    ```

--- src/spotify.cpp
+++ src/spotify.cpp
@@ -10,7 +10,7 @@
 #define API_REQUEST_CANCELED_NO_TOKEN -1
 
 // CN: *.spotify.com
-const char certSha1Fingerprint[] PROGMEM = "AB BC 7C 9B 7A D8 5D 98 8B B2 72 A4 4C 13 47 9A 00 2F 70 B5";
+const char certSha1Fingerprint[] PROGMEM = "B9 79 6B CE FD 61 21 97 A7 02 90 EE DA CD F0 A0 44 13 0E EB";
 
 // The OAuth scopes needed to do stuff
 // "user-read-playback-state", "user-modify-playback-state"
